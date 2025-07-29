acc6adea77badbe3fabd967dc4d5c7756848f6e8
Albin George <albingeorgerky@gmail.com>
2025-07-29 18:51:26 +0530

    Update Program01.java

--- Program01.java
+++ Program01.java
@@ -1,18 +1,11 @@
-<<<<<<< HEAD
-/*Program to print hello world */
-=======
 /*
 Program No:01
 Program to print hello world
 */
->>>>>>> add-programs
 class Program01 {
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
-<<<<<<< HEAD
-}
-=======
 }
 
 /*
@@ -20,4 +13,3 @@ Output
 ======
 Hello World!
  */
->>>>>>> add-programs
